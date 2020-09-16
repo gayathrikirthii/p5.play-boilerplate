9e22d783c99ce46e19a1c2729e083e5d0187b26a
gayathrikirthii <70528712+gayathrikirthii@users.noreply.github.com>
2020-09-16 10:54:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,36 @@
+const Engine= Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var object,ball;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400,400);
+  engine = Engine.create();
+  world=engine.world;
+
+  var opt={
+    isStatic:true
+  }
+  var ballopt={
+    restitution:3.0
+  }
+
+  object= Bodies.rectangle(200,390,200,20,opt)
+  World.add(world,object);
+  console.log(object)
+  
+  ball=Bodies.circle(200,100,20,ballopt);
+  World.add(world,ball);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(255,200,200);  
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(object.position.x,object.position.y,400,20)
+  //rect(200, 200, 50, 50);
+  ellipseMode(RADIUS);
+  ellipse(ball.position.x,ball.position.y,20,20)
 }
